7fd81a8188a785b7a4621a0dd3f4be7bf49f518c
Guillermo Alfonso Varela Chouciño <guillevch@gmail.com>
2026-02-12 00:09:26 +0100

    chore: add shadcn button

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import { Button } from "./components/ui/button";
+
   let count: number = $state(0)
   const increment = () => {
     count += 1
   }
 </script>
 
-<button onclick={increment}>
-  count is {count}
-</button>
+<Button onclick={increment}>
+count is {count}
+</Button>
